781bac4496397a97d987b74dbe3afea775d32abb
Kirill <kirillik95@yandex.ru>
2019-09-01 13:08:14 +0300

    feat(initial): initial commit. created chat with index page and rooms template

---
+++ Live_Chat/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+
+
+# Create your views here.
+
+def index(request):
+	return render(request, 'Live_Chat/index.html', {})
+
+
+def room(request, room_name):
+	return render(request, 'Live_Chat/room.html', {
+		'room_name_json': mark_safe(json.dumps(room_name))
+	})
---
+++ django_chat/routing.py
@@ -0,0 +1,6 @@
+# django_chat/routing.py
+from channels.routing import ProtocolTypeRouter
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+})
